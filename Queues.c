81763ca2b4da444f0b09039657f8be4e69048ef9
Vineeth <smoggysizzy@protonmail.com>
2021-01-14 19:06:45 +0530

    implmented and performed operations on circular queue and linear queue using array and linked list respectively

--- Queues.c
+++ Queues.c
@@ -66,3 +66,108 @@ void display(struct Queue queue)
 
 // Circular queue implemented using array
 // We, use rear=(rear+1)%size to it circular
+// The front index doest store any data
+struct CQueue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+// creating a circular queue is same as that of a linear queue
+// In queue, we initalise front and rear as 0 instead of -1(in case of linear queue)
+void createC(struct CQueue *queue)
+{
+    printf("Enter the size of the circular queue\n");
+    scanf("%d\n",&queue->size);
+    queue->Q=(int *)malloc(queue->size*sizeof(int));
+    queue->front=queue->rear=0;
+}
+
+// insertion in a circular queue
+void enqueueC(struct CQueue *queue,int data)
+{
+    if((queue->rear+1)%queue->size==queue->front)
+    {
+        printf("Queue is full");
+        return;
+    }
+    queue->rear=(queue->rear+1)%queue->size;
+    queue->Q[queue->rear]=data;
+}
+
+// deletion in a circular queue
+int dequeueC(struct CQueue *queue)
+{
+    if(queue->front==queue->rear)
+    {
+        printf("Queue is empty");
+        return -1;
+    }
+    queue->front=(queue->front+1)%queue->size;
+    return queue->Q[queue->front];
+}
+
+// Displaying the elements in a circular queue
+void displayC(struct CQueue queue)
+{
+    int i=queue.front+1;
+    do
+    {
+        printf("%d\n",queue.Q[i]);
+        i=(i+1)%queue.size;
+    }while(i!=(queue.rear+1)%queue.size);
+}
+
+// queue implented using linked list
+struct Node
+{
+    int data;
+    struct Node *next;
+}*front=NULL,*rear=NULL;
+
+// insertion in a queue
+void enqueueL(struct Node *p,int data)
+{
+    struct Node *t=(struct Node *)malloc(sizeof(struct Node));
+    if(t==NULL)
+    {
+        printf("Queue is full");
+        return;
+    }
+    t->data=data;
+    t->next=NULL;
+    if(front==NULL)
+    {
+        front=rear=t;
+        return;
+    }
+    rear->next=t;
+    rear=t;
+}
+
+//deletion in a queue
+int dequeueL(struct Node *p)
+{
+    int x;
+    if(front==NULL)
+    {
+        printf("Queue is empty");
+        return -1;
+    }
+    x=front->data;
+    front=front->next;
+    free(p);
+    return x;
+}
+
+// displaying of all the elements in a queue
+void displayL(struct Node *t)
+{
+    while(t!=NULL)
+    {
+        printf("%d\n",t->data);
+        t=t->next;
+    }
+}
